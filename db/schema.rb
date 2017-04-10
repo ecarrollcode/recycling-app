cb0ce90895ee6db801d58d5838543aa6c0ac9826
Evan Yates <eyates18@cmc.edu>
2017-04-10 15:54:58 -0700

    schema file update

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,69 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308022003) do
+ActiveRecord::Schema.define(version: 20170406033307) do
+
+  create_table "active_admin_comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "namespace"
+    t.text     "body",          limit: 65535
+    t.string   "resource_id",                 null: false
+    t.string   "resource_type",               null: false
+    t.string   "author_type"
+    t.integer  "author_id"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
+  end
+
+  create_table "admin_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "admins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
+  end
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer "category_id", null: false
-    t.string "name", null: false
-    t.string "info"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "category_id", null: false
+    t.string   "name",        null: false
+    t.string   "info"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "cities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer "city_id", null: false
-    t.string "name", null: false
-    t.string "website"
-    t.string "gps_coords"
+    t.integer  "city_id",    null: false
+    t.string   "name",       null: false
+    t.string   "website"
+    t.string   "gps_coords"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -42,31 +90,38 @@ ActiveRecord::Schema.define(version: 20170308022003) do
   end
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "item_id", null: false
-    t.string "name", null: false
-    t.string "category", null: false
-    t.string "bin", null: false
-    t.string "info"
-    t.string "cities"
-    t.string "img_path"
-    t.string "recycling_plants"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "item_id",          null: false
+    t.string   "name",             null: false
+    t.string   "category",         null: false
+    t.string   "bin",              null: false
+    t.string   "info"
+    t.string   "cities"
+    t.string   "img_path"
+    t.string   "recycling_plants"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
   create_table "mailing_list", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "user_id", null: false
-    t.string "name", null: false
-    t.string "email", null: false
+    t.string   "user_id",    null: false
+    t.string   "name",       null: false
+    t.string   "email",      null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "recycling_plants", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "plant_id", null: false
-    t.string "name", null: false
-    t.string "plant_hours"
-    t.string "gps_coords"
+    t.string   "plant_id",    null: false
+    t.string   "name",        null: false
+    t.string   "plant_hours"
+    t.string   "gps_coords"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "search_categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "sc_id",      null: false
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
