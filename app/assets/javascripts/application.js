79a62c5f00e847bb24df7a424b9d4f1bc690dcc7
Edward Carroll <e.carroll.code@gmail.com>
2017-04-24 01:08:01 -0700

    Began adding location functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -302,12 +302,47 @@ function overlay() {
   });
 }
 
+function getLocation() {
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else { 
+      console.log("Geolocation is not supported by this browser.");
+  }
+}
+
+function showPosition(position) {
+    console.log("Latitude: " + position.coords.latitude + 
+    " Longitude: " + position.coords.longitude);
+}
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+          console.log("User denied the request for Geolocation.");
+          break;
+        case error.POSITION_UNAVAILABLE:
+          console.log("Location information is unavailable.");
+          break;
+        case error.TIMEOUT:
+          console.log("The request to get user location timed out.");
+          break;
+        case error.UNKNOWN_ERROR:
+          console.log("An unknown error occurred.");
+          break;
+        default:
+          console.log("Request for user location successful!")
+          break;
+
+    }
+}
+
 function main() {
   $(document).ready(function() {
     filterItems();
     getSearch();
     navBarScrolling();
     overlay();
+    getLocation();
     addGoogleMapsAPI();
     initMap();
   });
